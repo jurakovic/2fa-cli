46c6139728b9c7b99fbc78c832c072326b725a86
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-10-06 23:26:54 +0200

    encrypt decrypt ok

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.CommandLine;
+using System.IO;
 using System.Linq;
-using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace _2fa
@@ -47,6 +51,69 @@ namespace _2fa
 				Console.WriteLine($"secret = {secret}");
 				Console.WriteLine($"--kind = {kind}");
 				Console.WriteLine($"--size = {size}");
+
+				Config config;
+				string password;
+
+				string userPath = Environment.GetEnvironmentVariable("USERPROFILE");
+				string file = Path.Combine(userPath, ".2fa-cli.json");
+				if (File.Exists(file))
+				{
+					string text = File.ReadAllText(file);
+					config = JsonSerializer.Deserialize<Config>(text);
+
+					Console.WriteLine("Enter password: ");
+					password = Console.ReadLine();
+
+					var tt = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+					string passhash = Convert.ToBase64String(tt);
+
+					if (passhash != config.PasswordHash)
+					{
+						Console.WriteLine("Wrong password");
+						// todo: exit 1
+					}
+
+					//Entry entry = config.Entries.FirstOrDefault(x => x.Name == name);
+					//var decry = AesOperation.DecryptString(password, entry.SecretEncrypted);
+				}
+				else
+				{
+					Console.WriteLine("Missing password");
+					Console.WriteLine("Enter new password: ");
+					password = Console.ReadLine();
+
+					config = new Config();
+					var tt = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+					config.PasswordHash = Convert.ToBase64String(tt);
+
+					File.WriteAllText(file, JsonSerializer.Serialize(config, new JsonSerializerOptions
+					{
+						WriteIndented = true
+					}));
+				}
+
+				Entry newEntry = new Entry()
+				{
+					Name = name,
+					SecretEncrypted = AesOperation.EncryptString(password, secret),
+					Type = EntryType.Totp, // todo
+					Size = size,
+				};
+
+				if (config.Entries == null)
+				{
+					config.Entries = new List<Entry>() { newEntry };
+				}
+				else
+				{
+					config.Entries.Add(newEntry);
+				}
+
+				File.WriteAllText(file, JsonSerializer.Serialize(config, new JsonSerializerOptions
+				{
+					WriteIndented = true
+				}));
 			},
 			nameArgument, secretArgument, kindOption, sizeOption);
 			rootCommand.Add(addCommand);
@@ -73,22 +140,94 @@ namespace _2fa
 
 	internal class Config
 	{
-		private string PasswordHash;
-		private Entry[] Entries;
+		public string PasswordHash { get; set; }
+		public List<Entry> Entries { get; set; }
 	}
 
 	internal class Entry
 	{
-		private string Name;
-		private string SecretEncrypted;
-		private SecureString Secret;
-		private EntryType Type;
-		private int Size;
+		public string Name { get; init; }
+		public string SecretEncrypted { get; init; }
+		//[JsonIgnore]
+		//public string Secret { get; init; }
+		public EntryType Type { get; init; }
+		public int Size { get; init; }
 	}
 
 	enum EntryType
 	{
 		Totp,
 		Hotp
+	}
+
+
+	public class AesOperation
+	{
+		// src: https://www.c-sharpcorner.com/article/encryption-and-decryption-using-a-symmetric-key-in-c-sharp/
+
+		public static string EncryptString(string key, string plainText)
+		{
+			byte[] iv = new byte[16];
+			byte[] array;
+
+			using (Aes aes = Aes.Create())
+			{
+				//aes.Key = Encoding.UTF8.GetBytes(key);
+				aes.Key = DeriveKeyFromPassword(key);
+				aes.IV = iv;
+
+				ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+				using (MemoryStream memoryStream = new MemoryStream())
+				{
+					using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+					{
+						using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+						{
+							streamWriter.Write(plainText);
+						}
+
+						array = memoryStream.ToArray();
+					}
+				}
+			}
+
+			return Convert.ToBase64String(array);
+		}
+
+		public static string DecryptString(string key, string cipherText)
+		{
+			byte[] iv = new byte[16];
+			byte[] buffer = Convert.FromBase64String(cipherText);
+
+			using (Aes aes = Aes.Create())
+			{
+				//aes.Key = Encoding.UTF8.GetBytes(key);
+				aes.Key = DeriveKeyFromPassword(key);
+				aes.IV = iv;
+				ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+				using (MemoryStream memoryStream = new MemoryStream(buffer))
+				{
+					using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+					{
+						using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+						{
+							return streamReader.ReadToEnd();
+						}
+					}
+				}
+			}
+		}
+
+		private static byte[] DeriveKeyFromPassword(string password)
+		{
+			// src: https://code-maze.com/csharp-string-encryption-decryption/
+			var emptySalt = Array.Empty<byte>();
+			var iterations = 1000;
+			var desiredKeyLength = 16; // 16 bytes equal 128 bits.
+			var hashMethod = HashAlgorithmName.SHA384;
+			return Rfc2898DeriveBytes.Pbkdf2(Encoding.Unicode.GetBytes(password), emptySalt, iterations, hashMethod, desiredKeyLength);
+		}
 	}
 }
