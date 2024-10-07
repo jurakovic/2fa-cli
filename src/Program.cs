359f17c4725dccbddd161ed2b5961a258428a0f2
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-10-07 23:37:47 +0200

    add exception handling

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.CommandLine;
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
 using System.Threading.Tasks;
 
 namespace _2fa
@@ -8,7 +11,20 @@ namespace _2fa
 		static async Task<int> Main(string[] args)
 		{
 			var rootCommand = CommandBuilder.CreateRootCommand();
-			return await rootCommand.InvokeAsync(args);
+
+			var builder = new CommandLineBuilder(rootCommand)
+				.UseDefaults()
+				.UseExceptionHandler((exception, context) =>
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine($"Error: {exception.Message}");
+					Console.ResetColor();
+
+					context.ExitCode = 1;
+				});
+
+			var parser = builder.Build();
+			return await parser.InvokeAsync(args);
 		}
 	}
 }
