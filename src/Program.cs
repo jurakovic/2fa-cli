1fc38d7cf41dd041501c3717be5e6c7e0d0fa51a
jurakovic <17744091+jurakovic@users.noreply.github.com>
2024-10-07 08:55:20 +0200

    Revert "test clifx"
    This reverts commit dd0e609fc0a151f5013bba97e7b9ad018c7ea495.

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,6 @@
-﻿using CliFx;
-using CliFx.Attributes;
-using CliFx.Infrastructure;
-using System;
+﻿using System;
 using System.Collections.Generic;
-//using System.CommandLine;
+using System.CommandLine;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -15,195 +12,130 @@ namespace _2fa
 {
 	internal class Program
 	{
-		public static async Task<int> Main()
+		static async Task Main(string[] args)
 		{
-			return await new CliApplicationBuilder()
-					.AddCommandsFromThisAssembly()
-					.Build()
-					.RunAsync();
+			var rootCommand = new RootCommand("2FA CLI tool");
+
+			var addCommand = new Command("add", "Add new entry");
+
+			var typeOption = new Option<string>(
+				name: "--type",
+				description: "OTP type",
+				getDefaultValue: () => "totp");
+			typeOption.FromAmong("totp", "hotp");
+			typeOption.AddAlias("-t");
+
+			var sizeOption = new Option<int>(
+				name: "--length",
+				description: "OTP length",
+				getDefaultValue: () => 6);
+			sizeOption.FromAmong(Enumerable.Range(4, 5).Select(x => x.ToString()).ToArray());
+			sizeOption.AddAlias("-l");
+
+			var secretArgument = new Argument<string>(
+				name: "secret",
+				description: "Secret key");
+
+			var nameArgument = new Argument<string>(
+				name: "name",
+				description: "Name");
+
+			addCommand.Add(nameArgument);
+			addCommand.Add(secretArgument);
+			addCommand.Add(typeOption);
+			addCommand.Add(sizeOption);
+
+			addCommand.SetHandler((name, secret, type, size) =>
+			{
+				Console.WriteLine($"name   = {name}");
+				Console.WriteLine($"secret = {secret}");
+				Console.WriteLine($"--type = {type}");
+				Console.WriteLine($"--size = {size}");
+
+				Config config;
+				string password;
+
+				string userPath = Environment.GetEnvironmentVariable("USERPROFILE");
+				string file = Path.Combine(userPath, ".2fa-cli.json");
+				if (File.Exists(file))
+				{
+					string text = File.ReadAllText(file);
+					config = JsonSerializer.Deserialize<Config>(text);
+
+					Console.WriteLine("Enter password: ");
+					password = Console.ReadLine();
+
+					var tt = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+					string passhash = Convert.ToBase64String(tt);
+
+					if (passhash != config.PasswordHash)
+					{
+						Console.WriteLine("Wrong password");
+						// todo: exit 1
+					}
+
+					//Entry entry = config.Entries.FirstOrDefault(x => x.Name == name);
+					//var decry = AesOperation.DecryptString(password, entry.SecretEncrypted);
+				}
+				else
+				{
+					Console.WriteLine("Missing password");
+					Console.WriteLine("Enter new password: ");
+					password = Console.ReadLine();
+
+					config = new Config();
+					var tt = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+					config.PasswordHash = Convert.ToBase64String(tt);
+
+					File.WriteAllText(file, JsonSerializer.Serialize(config, new JsonSerializerOptions
+					{
+						WriteIndented = true
+					}));
+				}
+
+				Entry newEntry = new Entry()
+				{
+					Name = name,
+					SecretEncrypted = AesOperation.EncryptString(password, secret),
+					Type = EntryType.Totp, // todo
+					Size = size,
+				};
+
+				if (config.Entries == null)
+				{
+					config.Entries = new List<Entry>() { newEntry };
+				}
+				else
+				{
+					config.Entries.Add(newEntry);
+				}
+
+				File.WriteAllText(file, JsonSerializer.Serialize(config, new JsonSerializerOptions
+				{
+					WriteIndented = true
+				}));
+			},
+			nameArgument, secretArgument, typeOption, sizeOption);
+			rootCommand.Add(addCommand);
+
+			var getCommand = new Command("get", "Get OTP");
+			getCommand.Add(nameArgument);
+			getCommand.SetHandler((name) =>
+			{
+				Console.WriteLine($"name   = {name}");
+			},
+			nameArgument);
+			rootCommand.Add(getCommand);
+
+			var listCommand = new Command("list", "List all entries");
+			listCommand.SetHandler(() =>
+			{
+				Console.WriteLine($"list...");
+			});
+			rootCommand.Add(listCommand);
+
+			await rootCommand.InvokeAsync(args);
 		}
-
-
-		//static async Task Main(string[] args)
-		//{
-		//	var rootCommand = new RootCommand("2FA CLI tool");
-		//
-		//	var addCommand = new Command("add", "Add new entry");
-		//
-		//	var typeOption = new Option<string>(
-		//		name: "--type",
-		//		description: "OTP type",
-		//		getDefaultValue: () => "totp");
-		//	typeOption.FromAmong("totp", "hotp");
-		//	typeOption.AddAlias("-t");
-		//
-		//	var sizeOption = new Option<int>(
-		//		name: "--length",
-		//		description: "OTP length",
-		//		getDefaultValue: () => 6);
-		//	sizeOption.FromAmong(Enumerable.Range(4, 5).Select(x => x.ToString()).ToArray());
-		//	sizeOption.AddAlias("-l");
-		//
-		//	var secretArgument = new Argument<string>(
-		//		name: "secret",
-		//		description: "Secret key");
-		//
-		//	var nameArgument = new Argument<string>(
-		//		name: "name",
-		//		description: "Name");
-		//
-		//	addCommand.Add(nameArgument);
-		//	addCommand.Add(secretArgument);
-		//	addCommand.Add(typeOption);
-		//	addCommand.Add(sizeOption);
-		//
-		//	addCommand.SetHandler((name, secret, type, size) =>
-		//	{
-		//		Console.WriteLine($"name   = {name}");
-		//		Console.WriteLine($"secret = {secret}");
-		//		Console.WriteLine($"--type = {type}");
-		//		Console.WriteLine($"--size = {size}");
-		//
-		//		Config config;
-		//		string password;
-		//
-		//		string userPath = Environment.GetEnvironmentVariable("USERPROFILE");
-		//		string file = Path.Combine(userPath, ".2fa-cli.json");
-		//		if (File.Exists(file))
-		//		{
-		//			string text = File.ReadAllText(file);
-		//			config = JsonSerializer.Deserialize<Config>(text);
-		//
-		//			Console.WriteLine("Enter password: ");
-		//			password = Console.ReadLine();
-		//
-		//			var tt = SHA256.HashData(Encoding.UTF8.GetBytes(password));
-		//			string passhash = Convert.ToBase64String(tt);
-		//
-		//			if (passhash != config.PasswordHash)
-		//			{
-		//				Console.WriteLine("Wrong password");
-		//				// todo: exit 1
-		//			}
-		//
-		//			//Entry entry = config.Entries.FirstOrDefault(x => x.Name == name);
-		//			//var decry = AesOperation.DecryptString(password, entry.SecretEncrypted);
-		//		}
-		//		else
-		//		{
-		//			Console.WriteLine("Missing password");
-		//			Console.WriteLine("Enter new password: ");
-		//			password = Console.ReadLine();
-		//
-		//			config = new Config();
-		//			var tt = SHA256.HashData(Encoding.UTF8.GetBytes(password));
-		//			config.PasswordHash = Convert.ToBase64String(tt);
-		//
-		//			File.WriteAllText(file, JsonSerializer.Serialize(config, new JsonSerializerOptions
-		//			{
-		//				WriteIndented = true
-		//			}));
-		//		}
-		//
-		//		Entry newEntry = new Entry()
-		//		{
-		//			Name = name,
-		//			SecretEncrypted = AesOperation.EncryptString(password, secret),
-		//			Type = EntryType.Totp, // todo
-		//			Size = size,
-		//		};
-		//
-		//		if (config.Entries == null)
-		//		{
-		//			config.Entries = new List<Entry>() { newEntry };
-		//		}
-		//		else
-		//		{
-		//			config.Entries.Add(newEntry);
-		//		}
-		//
-		//		File.WriteAllText(file, JsonSerializer.Serialize(config, new JsonSerializerOptions
-		//		{
-		//			WriteIndented = true
-		//		}));
-		//	},
-		//	nameArgument, secretArgument, typeOption, sizeOption);
-		//	rootCommand.Add(addCommand);
-		//
-		//	var getCommand = new Command("get", "Get OTP");
-		//	getCommand.Add(nameArgument);
-		//	getCommand.SetHandler((name) =>
-		//	{
-		//		Console.WriteLine($"name   = {name}");
-		//	},
-		//	nameArgument);
-		//	rootCommand.Add(getCommand);
-		//
-		//	var listCommand = new Command("list", "List all entries");
-		//	listCommand.SetHandler(() =>
-		//	{
-		//		Console.WriteLine($"list...");
-		//	});
-		//	rootCommand.Add(listCommand);
-		//
-		//	await rootCommand.InvokeAsync(args);
-		//}
-	}
-
-	[Command("add", Description = "Add new entry")]
-	public class AddCommand : ICommand
-	{
-		// Order: 0
-		[CommandParameter(0, Description = "Name")]
-		public required string Name { get; init; }
-
-		[CommandParameter(1, Description = "Secret key")]
-		public required string Secret { get; init; }
-
-		[CommandOption("type", 't', Description = "OTP type")]
-		public string Type { get; init; } = "totp";
-
-		[CommandOption("length", 'l', Description = "OTP length", Validators =)]
-		public int Length { get; init; } = 6;
-
-		public ValueTask ExecuteAsync(IConsole console)
-		{
-			console.Output.WriteLine($"name     = {Name}");
-			console.Output.WriteLine($"secret   = {Secret}");
-			console.Output.WriteLine($"--type   = {Type}");
-			console.Output.WriteLine($"--length = {Length}");
-
-			// If the execution is not meant to be asynchronous,
-			// return an empty task at the end of the method.
-			return default;
-		}
-
-		//var addCommand = new Command("add", "Add new entry");
-		//
-		//var typeOption = new Option<string>(
-		//	name: "--type",
-		//	description: "OTP type",
-		//	getDefaultValue: () => "totp");
-		//typeOption.FromAmong("totp", "hotp");
-		//	typeOption.AddAlias("-t");
-		//
-		//	var sizeOption = new Option<int>(
-		//		name: "--length",
-		//		description: "OTP length",
-		//		getDefaultValue: () => 6);
-		//sizeOption.FromAmong(Enumerable.Range(4, 5).Select(x => x.ToString()).ToArray());
-		//	sizeOption.AddAlias("-l");
-		//
-		//	var secretArgument = new Argument<string>(
-		//		name: "secret",
-		//		description: "Secret key");
-		//
-		//var nameArgument = new Argument<string>(
-		//	name: "name",
-		//	description: "Name");
-
-
-
 	}
 
 	internal class Config
